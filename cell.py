b673e6aadfeafd31c33b7bd8877b28a890f5b39a
Fabio Dantas Fernandes <fabio@Fabios-MacBook-Air.local>
2025-03-18 15:49:06 -0300

    Cell.py added

---
+++ cell.py
@@ -0,0 +1,27 @@
+from graphics import *
+
+class Cell():
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y1), Point(self._x1,self._y2)), "red")
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2,self._y1), Point(self._x2,self._y2)), "blue")
+        if self.has_top_wall: 
+            self._win.draw_line(Line(Point(self._x1,self._y1), Point(self._x2,self._y1)), "green")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "purple")
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800,600)
     # win.draw_line(Line(Point(50,50), Point(400,400)), "red")
     # win.draw_line(Line(Point(50,400), Point(400,50)), "blue")
     # win.draw_line(Line(Point(200,100), Point(200,500)), "green")
-    cell = Cell(50, 200, 50, 200, win)
-    cell.draw()
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
     
     
